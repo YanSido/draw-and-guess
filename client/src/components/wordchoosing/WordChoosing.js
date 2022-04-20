e9a2540582bf9258e2c54544043d808613ee820c
YanSido <yansido1@gmail.com>
2022-04-20 18:28:45 +0300

    word choosing view completed

--- client/src/components/wordchoosing/WordChoosing.js
+++ client/src/components/wordchoosing/WordChoosing.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { useState } from "react";
+import "./wordchoosing.css";
 
 export default function WordChoosing() {
-  return <div>WordChoosing</div>;
+  const [words, setWords] = useState(["hello", "world", "hell", "wall", "apple", "shaul"]);
+
+  return (
+    <div id="wordchoosing">
+      <h1 id="choose-title">Choose word to draw</h1>
+      <div class="grid-container">
+        {words.map((word, index) => {
+          return (
+            <button class="grid-item" id={index}>
+              {word}
+            </button>
+          );
+        })}
+      </div>
+      <button id="refresh-button">↻</button>
+    </div>
+  );
 }
