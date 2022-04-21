2a6d3b80b285dc0608687e796f1bc0b7777c2531
YanSido <yansido1@gmail.com>
2022-04-21 17:32:36 +0300

    server initial setup

--- app.js
+++ app.js
@@ -6,10 +6,31 @@ const bodyParser = require("body-parser");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const PORT = process.env.PORT || 8080;
+const { words } = require("./data/words.js");
+
+let rooms = {};
+
+const createUser = (id, username, roomId) => {
+  const user = { username: username, id };
+  rooms[roomId]["users"].push(user);
+};
+
+const createRoom = (roomId, socketId) => {
+  rooms[roomId] = {
+    users: [],
+    word: null,
+    score: 0,
+    firstDrawer: socketId,
+  };
+};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log(socket.id, "connected");
+});
 
 app.use("/", express.static(`./client/build`));
 
--- client/src/components/waiting/Waiting.js
+++ client/src/components/waiting/Waiting.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./waiting.css";
+import { Link } from "react-router-dom";
 
 export default function Waiting() {
   return (
     <div id="waiting">
+      <Link to={`/`}>
+        <button id="back-button">↩</button>
+      </Link>
       <div class="lds-ring">
         <div></div>
         <div></div>
